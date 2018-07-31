05067c38af1d8ca442fc2fce6858ee4cce7c6c87
nbcei <nbcei@nbceidemacbook-pro.local>
2018-07-31 10:48:41 +0800

    添加底部导航，以及如何适配8.0系统的配置

--- IOSDouYuZB/IOSDouYuZB/AppDelegate.swift
+++ IOSDouYuZB/IOSDouYuZB/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //这里是设置全局的tabBar底部导航栏的颜色
+        UITabBar.appearance().tintColor = UIColor.orange;
         return true
     }
 
